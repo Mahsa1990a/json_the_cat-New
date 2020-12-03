e6ddca8d4bc2ca1572edbbb74c58e67741fea15d
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-03 02:16:51 +0000

    search for sib breed, give back body(array)

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,8 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search', (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
-//result :
-// error: null
-// statusCode: 200
-// body: []
