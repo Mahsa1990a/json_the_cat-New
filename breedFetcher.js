6bad63f8aa6002322c67b0a0bfda148fc19871d4
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-03 19:57:38 +0000

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,37 +1,29 @@
 const request = require('request');
 
-let args = process.argv.slice(2);
-let breedName = args[0];
+//let args = process.argv.slice(2);
+//let breedName = args[0];
 
-//const fetchBreedDescription = function(breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     //console.log('error:', error); // Print the error if one occurred
     const data = JSON.parse(body);
-    console.log(data[0].description);
+    //console.log(data[0].description);
 
-    if (error) { //after changing url path
-      console.log("it is an error", error);
+    if (error) { //there is error: so error, null
+      callback(error, null);
+    } else if (data[0]) { //there is not error so: null, desc of body
+      callback(null, data[0].description);
     }
 
     if (data.length === 0) {
       console.log('The breed you entered was not found');
     } else if (response.statusCode !== 200) {
       console.log(`API call failed with statusCode: ${response.statusCode}`);
-    }  
+    }
   
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body); // Print the HTML for the Google homepage.
-    //console.log("typeof body is: ", typeof body) //string
-    //Inorder to scan this data like JavaScript object:
-  
-    //console.log("data is: ", data); //[{ ... }]
-    //console.log("data[0] is: ", data[0].name); // { ... }
-    //console.log("type of data: ", typeof data); //object
-    //console.log(data[0].description); //prtint cat description //node breedFetcher.js Chartreux
   });
 
-//}
-
-//module.exports = { fetchBreedDescription };
+};
+module.exports = { fetchBreedDescription };
 
 
