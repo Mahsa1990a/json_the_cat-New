32fa3696460be34a49ca9a9924b6a102244f1b20
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-03 02:35:43 +0000

    Dynamic Breed Name

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+let args = process.argv.slice(2);
+let name = args[0];
+request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log("typeof body is: ", typeof body) //string
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log("typeof body is: ", typeof body) //string
 
   //Inorder to scan this data like JavaScript object:
   const data = JSON.parse(body);
-  console.log("data is: ", data); //[{ ... }]
-  console.log("data[0] is: ", data[0]); // { ... }
-  console.log("type of data: ", typeof data); //object
+  //console.log("data is: ", data); //[{ ... }]
+  //console.log("data[0] is: ", data[0]); // { ... }
+  //console.log("type of data: ", typeof data); //object
+
+  console.log(data[0].description);
 });
 
